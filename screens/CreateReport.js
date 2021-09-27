e8b2cd705b6e5681b03a20097a0464296049ba4f
Alfredo Ortegón Sepúlveda <alorse@gmail.com>
2021-09-27 16:07:09 -0500

    Logic to render forms

--- screens/CreateReport.js
+++ screens/CreateReport.js
@@ -13,15 +13,20 @@ import { Block, theme } from "galio-framework";
 import schema from '../assets/config/schemas';
 import uiSchema from '../assets/config/uischemas';
 import { JsonFormsStyleContext } from '@jsonforms/vanilla-renderers';
+import { eq } from "react-native-reanimated";
 
 const { width, height } = Dimensions.get("screen");
 const data = {
-  "Equipos": [
-  {
-    "Equipo": "CELDA SECCIONADOR",
-    "Cantidad": 1,
-  }
-]};
+  "Fecha": new Date(),
+  equipos: [
+    {
+      tipo: "CELDA SECCIONADOR",
+      cantidad: 2,
+    }
+  ]
+};
+var tales = schema.hv
+var uiTales = uiSchema.hv
 
 function CreateReportScreen() {
   return (
@@ -33,17 +38,42 @@ function CreateReportScreen() {
   );
 }
 
+function getDataForm(data) {
+  var equipos = {};
+  if(data.equipos.length > 0) {
+    equipos = data.equipos
+    // Reset Forms
+    uiTales.elements[1].elements = []
+    equipos.map((equipo) => {
+      if(equipo.tipo !== undefined) {
+        renderEquipos(equipo, "CELDA SECCIONADOR", schema.seccionador, uiSchema.seccionador)
+        renderEquipos(equipo, "CELDA DOBLE TIRO PRIMARIO", schema.doble_tiro_primario, uiSchema.doble_tiro_primario)
+      }
+    })
+  }
+}
+
+function renderEquipos(equipo, type, shema, uiShema,) {
+  var cantidad = equipo.cantidad !== undefined ? equipo.cantidad : 0
+  if(equipo.tipo == type) {
+    for(var i=0; i < cantidad; i++) {
+      tales.properties = {...tales.properties, ...shema}
+      uiTales.elements[1].elements.push({...uiSchema.hv.elements[1].elements, ...uiShema})
+    }
+  }
+}
 function renderCards() {
-  const [data2, setData] = useState();
+  // const [data, setData] = useState(data);
+
   return (
     <JsonFormsStyleContext.Provider value={styleContextValue}>
       <JsonForms
-        schema={schema.hv}
-        uischema={uiSchema.hv}
+        schema={tales}
+        uischema={uiTales}
         data={data}
         renderers={materialRenderers}
         cells={materialCells}
-        onChange={({ data, _errors }) => setData(data)}
+        onChange={({ data, _errors }) => getDataForm(data)}
       />
     </JsonFormsStyleContext.Provider>
   );
